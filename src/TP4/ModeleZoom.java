ba32b40760e0a65368c79a74297d9b03a31ece94
bouchard.claude <bouchard.claude@live.ca>
2009-11-27 19:35:04 +0000

    Ajout de ModelZoom

---
+++ src/TP4/ModeleZoom.java
@@ -0,0 +1,58 @@
+package TP4;
+
+import java.util.ArrayList;
+import java.util.Observable;
+
+/**
+ * @author CB / TEAM CHJG
+ * @version 1.0
+ * @created 27-nov.-2009 14:26:27
+ */
+public class ModeleZoom extends Observable {
+
+	private static ModeleZoom instance = new ModeleZoom();
+	private ArrayList<Vue> lavues;
+	private int zoom;
+
+	private ModeleZoom()
+	{
+
+	}
+
+	/**
+	 * 
+	 * @param 1
+	 */
+	public void addVue(Vue uneVue)
+	{
+
+	}
+
+	public static ModeleZoom getInstance()
+	{
+		return instance;
+	}
+
+	public int getZoom()
+	{
+		return 0;
+	}
+
+	/**
+	 * 
+	 * @param 1
+	 */
+	public void removeVue(Vue uneVue)
+	{
+
+	}
+
+	/**
+	 * 
+	 * @param 1
+	 */
+	public void setZoom(int unZoom){
+
+	}
+
+}
