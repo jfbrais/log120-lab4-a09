5d3748bcf8034b64a6fca3d0ddf9bb734d16b9d0
bouchard.claude@live.ca <bouchard.claude@live.ca@1709877c-db89-11de-8f20-5962d2c82ea8>
2009-11-27 19:25:56 +0000

    Ajout de la classe Target

--- src/TP4/Target.java
+++ src/TP4/Target.java
@@ -1,6 +1,70 @@
 package TP4;
 
-public class Target
-{
+/**
+ * @author CB / TEAM CHJB
+ * @version 1.0
+ * @created 27-nov.-2009 14:20:25
+ */
+public class Target {
+
+	public Target()
+	{
+
+	}
+
+	public BufferedImage getImage()
+	{
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param 1
+	 */
+	public void setImage(BufferedImage uneImage)
+	{
+
+	}
+
+	public int getX()
+	{
+		return 0;
+	}
+
+	public int getY()
+	{
+		return 0;
+	}
+
+	public int getZoom()
+	{
+		return 0;
+	}
+
+
+
+	/**
+	 * 
+	 * @param 1
+	 */
+	public void setX(int leX){
+
+	}
+
+	/**
+	 * 
+	 * @param 1
+	 */
+	public void setY(int leY){
+
+	}
+
+	/**
+	 * 
+	 * @param 1
+	 */
+	public void setZoom(int leZoom){
+
+	}
 
 }
