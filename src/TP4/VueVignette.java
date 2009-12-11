ab0e2f33a72a28d67a0de825056980b2cb9d5b87
bouchard.claude <bouchard.claude@live.ca>
2009-12-11 03:28:04 +0000

    HEAD!

--- src/TP4/VueVignette.java
+++ src/TP4/VueVignette.java
@@ -1,3 +1,29 @@
+/******************************************************
+ Cours :             LOG120
+ Session :           Saison automne 2009
+ Groupe :            03
+ Projet :            Laboratoire #4
+ Étudiant(e)(s) :    Gabriel Desmarais
+ 					 Jean-François Brais-Villemur
+ 					 Claude Bouchard
+ 					 Hugo Bergeron
+ Code(s) perm. :     DESG24078908
+ 					 BRAJ14088901
+ 					 BOUC12018902
+ 					 BERH17028709
+ Professeur :        Jocelyn Benoît
+ Chargé de labo.:    Vincent Carrier
+ 					 Yanick Rochon
+ Nom du fichier :	 VueVignette.java 					 
+ Date créée :        2009-12-10
+ Date dern. modif. : 2009-12-10
+ 
+ *******************************************************
+ Historique des modifications
+ *******************************************************
+ 2009-12-10 : Status Initial
+ *******************************************************/
+
 package TP4;
 
 import java.awt.AlphaComposite;
